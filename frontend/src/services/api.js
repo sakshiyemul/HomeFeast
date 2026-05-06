9bc056c865287354f187081d53488202bc92af78
unknown <sakshiyemul1524@gmail.com>
2026-05-06 22:18:54 +0530

    Updated API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./session";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api"
+  baseURL: process.env.REACT_APP_API_URL || "https://homefeast-backend.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
